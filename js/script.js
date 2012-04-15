6a0cf402c7644c0999e5bb31c9b352b21a0211f5
Jeremy Worboys <jeremy@complexcompulsions.com>
2012-04-16 09:12:19 +1000

    Added Sharre init script

--- js/script.js
+++ js/script.js
@@ -75,3 +75,29 @@ if (Modernizr.touch) {
 	w.addEventListener( "devicemotion", checkTilt, false );
 
 })( this );
+
+$('#shareme').sharrre({
+	share: {
+		googlePlus: true,
+		facebook: true,
+		twitter: true,
+		digg: true,
+		delicious: true,
+		stumbleupon: true,
+		linkedin: true,
+		pinterest: true
+	},
+	buttons: {
+		googlePlus: {size: 'tall'},
+		facebook: {layout: 'box_count'},
+		twitter: {count: 'vertical'},
+		digg: {type: 'DiggMedium'},
+		delicious: {size: 'tall'},
+		stumbleupon: {layout: '5'},
+		linkedin: {counter: 'top'},
+		pinterest: {media: 'http://sharrre.com/img/example1.png', description: $('#shareme').data('text'), layout: 'vertical'}
+	},
+	enableHover: false,
+	enableCounter: false,
+	enableTracking: true
+});
